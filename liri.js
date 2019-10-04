6ec69a61455c2a39ecf5345098dc622f06d93514
Randall <rfields1128@yahoo.com>
2019-10-03 22:37:52 -0500

    uncommented out the working spotify api caller, working bands in town api

--- liri.js
+++ liri.js
@@ -4,17 +4,17 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var axios = require("axios");
-
-// spotify.search({ type: 'track', query: 'Sem Amor - big band' })
-// .then(function(response) {
-//     console.log(response.tracks.items[0].artists[0].name);
-//     console.log(response.tracks.items[0].name); 
-//     console.log(response.tracks.items[0].album.name); 
-//     console.log(response.tracks.items[0].preview_url); 
-// })
-// .catch(function(err) {
-//     console.log(err);
-// });
+var 
+spotify.search({ type: 'track', query: 'Sem Amor - big band' })
+.then(function(response) {
+    console.log(response.tracks.items[0].artists[0].name);
+    console.log(response.tracks.items[0].name); 
+    console.log(response.tracks.items[0].album.name); 
+    console.log(response.tracks.items[0].preview_url); 
+})
+.catch(function(err) {
+    console.log(err);
+});
 
 var bandsintown = "https://rest.bandsintown.com/artists/thundercat/events?app_id=codingbootcamp"
 
