6856f1cf723c2669471847fe15744f82a7c736d0
Randall <rfields1128@yahoo.com>
2019-10-03 23:26:22 -0500

    added userInput and userCommand to use arguments in terminal for search terms, implemented a switch to differentiate the functions with an argument

--- liri.js
+++ liri.js
@@ -4,8 +4,43 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var axios = require("axios");
-var 
-spotify.search({ type: 'track', query: 'Sem Amor - big band' })
+var userCommand = process.argv[2]
+var userInput = process.argv[3]
+
+for (var i = 4; i < process.argv.length; i++) {
+    if (i > 4 && i < process.argv.length) {
+        userInput += "+" + process.argv[i];
+    }
+    else {
+        userInput += process.argv[i];
+    }
+}
+
+switch (userCommand) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "do-what-it-says":
+        doThis();
+        break;
+    default:
+        console.log("Please enter a valid search term");
+        break;
+}
+
+
+
+function spotifyThis() {
+    if (!userInput){
+        userInput = "The Sign Ace of Base";
+    }
+spotify.search({type: "track", query: userInput})
 .then(function(response) {
     console.log(response.tracks.items[0].artists[0].name);
     console.log(response.tracks.items[0].name); 
@@ -15,9 +50,10 @@ spotify.search({ type: 'track', query: 'Sem Amor - big band' })
 .catch(function(err) {
     console.log(err);
 });
+};
 
-var bandsintown = "https://rest.bandsintown.com/artists/thundercat/events?app_id=codingbootcamp"
-
+function concertThis() {
+var bandsintown = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
 axios.get(bandsintown)
 .then(function(response){
     for(var i = 0; i < response.data.length; i++) {
@@ -29,5 +65,20 @@ axios.get(bandsintown)
 .catch(function(error){
     console.log(error);
 })
+}
+function movieThis(){
+    axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy")
+    .then(function(response){
+        console.log("Title: " + response.data.Title);
+        console.log("Year Released: " + response.data.Year);
+        console.log("IMDB rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country/Countries Produced: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Cast: " + response.data.Actors);
+    });
+};
+
 
 console.log("Hello");
