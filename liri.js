3cef0ec28eb7f6b5c32124382d14d9bfa3100f3f
Randall <rfields1128@yahoo.com>
2019-10-03 22:36:17 -0500

    working spotify api caller

--- liri.js
+++ liri.js
@@ -5,15 +5,29 @@ var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var axios = require("axios");
 
-spotify.search({ type: 'track', query: 'Sem Amor - big band' })
-.then(function(response) {
-    console.log(response.tracks.items[0].artists[0].name);
-    console.log(response.tracks.items[0].name); 
-    console.log(response.tracks.items[0].album.name); 
-    console.log(response.tracks.items[0].preview_url); 
+// spotify.search({ type: 'track', query: 'Sem Amor - big band' })
+// .then(function(response) {
+//     console.log(response.tracks.items[0].artists[0].name);
+//     console.log(response.tracks.items[0].name); 
+//     console.log(response.tracks.items[0].album.name); 
+//     console.log(response.tracks.items[0].preview_url); 
+// })
+// .catch(function(err) {
+//     console.log(err);
+// });
+
+var bandsintown = "https://rest.bandsintown.com/artists/thundercat/events?app_id=codingbootcamp"
+
+axios.get(bandsintown)
+.then(function(response){
+    for(var i = 0; i < response.data.length; i++) {
+        console.log("Venue:" + response.data[i].venue.name)
+        console.log("Venue Location:" + response.data[i].venue.city + "," + response.data[i].venue.country)
+        console.log("Date:" + moment(response.data[i].datetime).format("L"));
+    }
 })
-.catch(function(err) {
-    console.log(err);
-});
+.catch(function(error){
+    console.log(error);
+})
 
 console.log("Hello");
